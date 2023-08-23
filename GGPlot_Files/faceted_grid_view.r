29bf1fedce3c565bb7b36c309e1282f08dd1c62a
ravi3990 <94464194+ravi3990@users.noreply.github.com>
2023-08-23 23:59:21 +0100

    Create faceted_grid_view.r

---
+++ GGPlot_Files/faceted_grid_view.r
@@ -0,0 +1,15 @@
+cols2<- c("#ca0020","#0571b0")
+
+ggplot(barley) +
+  geom_density(aes(x = yield, fill = year, colour = year),  alpha = 0.3) +
+  
+  facet_grid(rows = vars(site)) +
+  
+  scale_color_manual(values = darken(cols2, 0.3) , name = "Year") +
+  
+  scale_fill_manual(values = cols2, name = "Year")  + theme_minimal() +
+  theme(
+    legend.position = "bottom", 
+    legend.justification = "left",  # Position the legend at bottom left
+    axis.line.x = element_blank(),
+    panel.grid.major = element_blank()  )
