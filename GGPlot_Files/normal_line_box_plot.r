c8c7b4b3aa7b39bac4c4e070abc3d4cc4c2fc62d
ravi3990 <94464194+ravi3990@users.noreply.github.com>
2023-08-24 22:59:13 +0100

    Create normal_line_box_plot.r

---
+++ GGPlot_Files/normal_line_box_plot.r
@@ -0,0 +1,25 @@
+ggplot(heights, aes(y=height, x=gender)) +
+  
+  geom_boxplot(colour = "gray30", show.legend=F, outlier.colour = NA, varwidth = TRUE, scale = "count") +
+  
+  geom_point( alpha = 0.15)+
+  
+  scale_y_continuous(breaks=seq(130,210, by =10))+
+  
+  coord_flip() +
+  
+  xlab("centimetres") +
+  
+  theme_minimal() +
+  
+  theme(
+    axis.text.y = element_text(size =10,face = "bold"),
+    axis.ticks.y =  element_blank(),
+    axis.title.y = element_blank(),
+    axis.title.x = element_text(size = 10),
+    axis.line.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.line.y = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.grid.major.x = element_line(size=0.2))
